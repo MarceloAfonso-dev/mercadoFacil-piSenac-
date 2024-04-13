e87d6f59352cd83dcdf443913bc88c2b9920b21e
Italo <italodeyvd2812@gmail.com>
2024-04-12 22:54:41 -0300

    feature: menu inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		int opcao;
+
+		System.out.println(
+				"Informe a opção desejada \n\n1. Cadastrar Empresa\n2. Cadastrar Funcionario \n3. Cadastrar Cliente \n4. Sair");
+		opcao = ler.nextInt();
+
+		switch (opcao) {
+		case 1, 2, 3 -> {
+
+			String doc, nome;
+
+			System.out.println("Informe o numero de identidade:");
+			doc = ler.next();
+			System.out.println("Informe o nome:");
+			nome = ler.nextLine();
+
+			System.out.println("Dados recebidos");
+			System.out.println(nome + doc);
+
+		}
+
+		default -> {
+
+			if (opcao == 4) {
+				System.out.println("Saindo");
+			}
+
+			else {
+				System.out.println("Opção invalida, por favor selecione outra opção.");
+			}
+
+		}
+		}
+	}
 }
