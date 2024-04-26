c8ce810a885d86fa64f368bc6cf22ff4beab6d14
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-04-25 23:56:03 -0300

    feature: Título de sistema ao iniciar

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
 		Scanner leitor = new Scanner(System.in);
 		Empresa empresa = new Empresa();
+		System.out.println("\r\n"
+				+ "  __  __                        _         ______ __       _ _ \r\n"
+				+ " |  \\/  |                      | |       |  ____/_/      (_) |\r\n"
+				+ " | \\  / | ___ _ __ ___ __ _  __| | ___   | |__  __ _  ___ _| |\r\n"
+				+ " | |\\/| |/ _ \\ '__/ __/ _` |/ _` |/ _ \\  |  __|/ _` |/ __| | |\r\n"
+				+ " | |  | |  __/ | | (_| (_| | (_| | (_) | | |  | (_| | (__| | |\r\n"
+				+ " |_|  |_|\\___|_|  \\___\\__,_|\\__,_|\\___/  |_|   \\__,_|\\___|_|_|\r\n"
+				+ "                                                              \r\n"
+				+ "                                                              \r\n"
+				+ "");
 		
 		if (!empresa.isCadastrado()) {
 			System.out.println("Seja bem vindo!\nInsira o nome e CNPJ da empresa:");
