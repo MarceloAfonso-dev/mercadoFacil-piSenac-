c8bf9e054178540d20f8294f7374bd22e86e5b14
Joaquim <joaquim.jabs0071@Gmail.com>
2024-04-28 14:45:45 -0300

    Fornecedores, Produtos

---
+++ src/Fornecedores.java
@@ -0,0 +1,78 @@
+public class Fornecedores {
+	private String fornecedores;
+    private String empresas;
+    private String distribuicaoDeProdutos;
+    private String controleDeVendas;
+    private int idDaEmpresa;
+    private String atacados;
+    private String varejos;
+    private String industrias;
+    private String laticinios;
+    private String frigorificos;
+	
+    public String getFornecedores() {
+		return fornecedores;
+	}
+	public void setFornecedores(String fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+	public String getEmpresas() {
+		return empresas;
+	}
+	public void setEmpresas(String empresas) {
+		this.empresas = empresas;
+	}
+	public String getDistribuicaoDeProdutos() {
+		return distribuicaoDeProdutos;
+	}
+	public void setDistribuicaoDeProdutos(String distribuicaoDeProdutos) {
+		this.distribuicaoDeProdutos = distribuicaoDeProdutos;
+	}
+	public String getControleDeVendas() {
+		return controleDeVendas;
+	}
+	public void setControleDeVendas(String controleDeVendas) {
+		this.controleDeVendas = controleDeVendas;
+	}
+	public int getIdDaEmpresa() {
+		return idDaEmpresa;
+	}
+	public void setIdDaEmpresa(int idDaEmpresa) {
+		this.idDaEmpresa = idDaEmpresa;
+	}
+	public String getAtacados() {
+		return atacados;
+	}
+	public void setAtacados(String atacados) {
+		this.atacados = atacados;
+	}
+	public String getVarejos() {
+		return varejos;
+	}
+	public void setVarejos(String varejos) {
+		this.varejos = varejos;
+	}
+	public String getIndustrias() {
+		return industrias;
+	}
+	public void setIndustrias(String industrias) {
+		this.industrias = industrias;
+	}
+	public String getLaticinios() {
+		return laticinios;
+	}
+	public void setLaticinios(String laticinios) {
+		this.laticinios = laticinios;
+	}
+	public String getFrigorificos() {
+		return frigorificos;
+	}
+	public void setFrigorificos(String frigorificos) {
+		this.frigorificos = frigorificos;
+	}
+    
+    
+    
+}
+	
+
---
+++ src/Produto.java
@@ -0,0 +1,43 @@
+public class Produto {
+	private int idProduto;
+    private String nomeProduto;
+    private int quantidadeProduto;
+    private double valorProduto;
+    private String categoriaProduto;
+	
+    public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public int getQuantidadeProduto() {
+		return quantidadeProduto;
+	}
+	public void setQuantidadeProduto(int quantidadeProduto) {
+		this.quantidadeProduto = quantidadeProduto;
+	}
+	public double getValorProduto() {
+		return valorProduto;
+	}
+	public void setValorProduto(double valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+	public String getCategoriaProduto() {
+		return categoriaProduto;
+	}
+	public void setCategoriaProduto(String categoriaProduto) {
+		this.categoriaProduto = categoriaProduto;
+	}
+    
+
+    
+   
+    }
+
