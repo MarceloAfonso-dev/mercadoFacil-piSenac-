dd59c6c99f2147bb02220ca3ec7e479490fb09cc
Italo <italodeyvd2812@gmail.com>
2024-05-02 20:03:45 -0300

    fix: projeto maven

---
+++ src/Cliente.java
@@ -0,0 +1,63 @@
+import java.time.LocalDate;
+
+public class Cliente {
+
+	private String nome;
+	private String cpf;
+	private String cep;
+	private String telefone;
+	private String idCliente;
+	private LocalDate dataNascimento;
+
+	public Cliente() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(String idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
