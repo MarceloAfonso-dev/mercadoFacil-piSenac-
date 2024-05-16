5db944283dce5b7cb5eabfbc178aca898f5d8b46
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-05-16 14:57:14 -0300

    feature: classe item_venda

--- src/classes/VendaItem.java
+++ src/classes/VendaItem.java
@@ -1,5 +1,64 @@
 package classes;
 
 public class VendaItem {
+	private String idItemVenda;
+	private String idVenda;
+	private String idProduto;
+	private int quantidade;
+	private double valorItem;
+	
+	public VendaItem() {
+		
+	}
 
+	public VendaItem(String idItemVenda, String idVenda, String idProduto, int quantidade, double valorItem) {
+		super();
+		this.idItemVenda = idItemVenda;
+		this.idVenda = idVenda;
+		this.idProduto = idProduto;
+		this.quantidade = quantidade;
+		this.valorItem = valorItem;
+	}
+
+	public String getIdItemVenda() {
+		return idItemVenda;
+	}
+
+	public void setIdItemVenda(String idItemVenda) {
+		this.idItemVenda = idItemVenda;
+	}
+
+	public String getIdVenda() {
+		return idVenda;
+	}
+
+	public void setIdVenda(String idVenda) {
+		this.idVenda = idVenda;
+	}
+
+	public String getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(String idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValorItem() {
+		return valorItem;
+	}
+
+	public void setValorItem(double valorItem) {
+		this.valorItem = valorItem;
+	}
+	
+	
 }
