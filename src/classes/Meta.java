5cc4be25c051ab8b96f60539631cc8dcb03f97f7
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-06-01 20:03:06 -0300

    feature: preenchendo dados de funcionarios no excel

--- src/classes/Meta.java
+++ src/classes/Meta.java
@@ -1,62 +1,101 @@
 package classes;
-import java.time.LocalDate;
+import java.util.Date;
 
 public class Meta {
 
 	private String idMeta;
 	private double valorMeta;
 	private double bonificacao;
-	private String idVendedor;
+	private String idFuncionario;
 	private String idCaixa;
-	private LocalDate dataInicio;
-	private LocalDate dataFinal;
+	private Date dataInicio;
+	private Date dataFinal;
 	private double valorVendas;
+	
+	public Meta() {	}
+	
+	public Meta(String idMeta, double valorMeta, double bonificacao, String idFuncionario, String idCaixa,
+			Date dataInicio, Date dataFinal, double valorVendas) {
+		super();
+		this.idMeta = idMeta;
+		this.valorMeta = valorMeta;
+		this.bonificacao = bonificacao;
+		this.idFuncionario = idFuncionario;
+		this.idCaixa = idCaixa;
+		this.dataInicio = dataInicio;
+		this.dataFinal = dataFinal;
+		this.valorVendas = valorVendas;
+	}
+
 	public String getIdMeta() {
 		return idMeta;
 	}
+
 	public void setIdMeta(String idMeta) {
 		this.idMeta = idMeta;
 	}
+
 	public double getValorMeta() {
 		return valorMeta;
 	}
+
 	public void setValorMeta(double valorMeta) {
 		this.valorMeta = valorMeta;
 	}
+
 	public double getBonificacao() {
 		return bonificacao;
 	}
+
 	public void setBonificacao(double bonificacao) {
 		this.bonificacao = bonificacao;
 	}
-	public String getIdVendedor() {
-		return idVendedor;
+
+	public String getIdFuncionario() {
+		return idFuncionario;
 	}
-	public void setIdVendedor(String idVendedor) {
-		this.idVendedor = idVendedor;
+
+	public void setIdFuncionario(String idFuncionario) {
+		this.idFuncionario = idFuncionario;
 	}
+
 	public String getIdCaixa() {
 		return idCaixa;
 	}
+
 	public void setIdCaixa(String idCaixa) {
 		this.idCaixa = idCaixa;
 	}
-	public LocalDate getDataInicio() {
+
+	public Date getDataInicio() {
 		return dataInicio;
 	}
-	public void setDataInicio(LocalDate dataInicio) {
+
+	public void setDataInicio(Date dataInicio) {
 		this.dataInicio = dataInicio;
 	}
-	public LocalDate getDataFinal() {
+
+	public Date getDataFinal() {
 		return dataFinal;
 	}
-	public void setDataFinal(LocalDate dataFinal) {
+
+	public void setDataFinal(Date dataFinal) {
 		this.dataFinal = dataFinal;
 	}
+
 	public double getValorVendas() {
 		return valorVendas;
 	}
+
 	public void setValorVendas(double valorVendas) {
 		this.valorVendas = valorVendas;
 	}
+
+	@Override
+	public String toString() {
+		return "Meta [idMeta=" + idMeta + ", valorMeta=" + valorMeta + ", bonificacao=" + bonificacao
+				+ ", idFuncionario=" + idFuncionario + ", idCaixa=" + idCaixa + ", dataInicio=" + dataInicio
+				+ ", dataFinal=" + dataFinal + ", valorVendas=" + valorVendas + "]";
+	}
+	
 }
--- src/gerenciadores/GerenciadorMeta.java
+++ src/gerenciadores/GerenciadorMeta.java
@@ -1,5 +1,101 @@
 package gerenciadores;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import classes.Meta;
+
 public class GerenciadorMeta {
-	
+	static Scanner leitorInterno = new Scanner(System.in);
+    static String fileName = ExcelLocalizacao.getFilename();
+
+    public static List<Meta> listarMetas() throws IOException {
+        List<Meta> listaMetas = new ArrayList<Meta>();
+
+        try {
+            FileInputStream arquivo = new FileInputStream(new File(fileName));
+            XSSFWorkbook workbook = new XSSFWorkbook(arquivo);
+            XSSFSheet sheetMetas = workbook.getSheetAt(5);
+
+            Iterator<Row> rowIterator = sheetMetas.iterator();
+            
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                if (row.getRowNum() == 0) {
+                    // Ignorar a primeira linha
+                    continue;
+                }
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                Meta meta = new Meta();
+                listaMetas.add(meta);
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getColumnIndex()) {
+                    case 0:
+                        meta.setIdMeta(String.valueOf(cell.getStringCellValue()));
+                        break;
+                    case 1:
+                    	meta.setValorMeta(cell.getNumericCellValue());
+                        break;
+                    case 2:
+                    	meta.setBonificacao(cell.getNumericCellValue());
+                        break;
+                    case 3:
+                    	meta.setIdFuncionario(String.valueOf(cell.getStringCellValue()));
+                        break;
+                    case 4:
+                        meta.setIdCaixa(String.valueOf(cell.getStringCellValue()));
+                        break;
+                    case 5:
+                        Date dataInicio = cell.getDateCellValue();
+                        meta.setDataInicio(dataInicio);
+                        break;
+                    case 6:
+                        Date dataFinal = cell.getDateCellValue();
+                        meta.setDataFinal(dataFinal);
+                        break;
+                    case 7:
+                    	meta.setValorVendas(cell.getNumericCellValue());
+                        break;
+                    }
+                }
+            }
+            arquivo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Arquivo Excel não encontrado!");
+        }
+        return listaMetas;
+    }
+
+    public static void verificarQuantidadeGeralMetas() throws IOException {
+        List<Meta> listaMeta = listarMetas();
+        
+        if (listaMeta.size() == 0) {
+            System.out.println("Nenhuma meta encontrada!");
+        } else {
+            System.out.println("Número total de metas: " + listaMeta.size());
+        }
+    }
+
+    public static void imprimirMetasGerais() throws IOException {
+    	List<Meta> listaMeta = listarMetas();
+        
+        for (Meta meta : listaMeta) {
+            System.out.println(meta.toString());
+        }
+    }
 }
